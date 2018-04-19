acd527219d908d626ab6b6f64c034e5ed6eb07a0
nishon.tan <nishon.tan@gmail.com>
2018-04-19 16:44:29 +0545

    Adds pixel arrow

--- app/src/main/java/np/com/naxa/vso/HomeActivity.java
+++ app/src/main/java/np/com/naxa/vso/HomeActivity.java
@@ -1,6 +1,8 @@
 package np.com.naxa.vso;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -10,7 +12,14 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.github.zagum.expandicon.ExpandIconView;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,6 +35,12 @@ public class HomeActivity extends AppCompatActivity {
     @BindView(R.id.recylcer_view_map_categories)
     RecyclerView recyclerView;
 
+    @BindView(R.id.map)
+    MapView mapView;
+
+    @BindView(R.id.expand_icon_view)
+    ExpandIconView expandIconView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,15 +48,54 @@ public class HomeActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setupRecyclerView();
         setupSlidingPanel();
+        setupMap();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+//        vMapView.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        Configuration.getInstance().save(this, prefs);
+        // vMapView.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
+
+    private void setupMap() {
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+        IMapController mapController = mapView.getController();
+        mapController.setZoom(12);
+        GeoPoint startPoint = new GeoPoint(27.716278, 85.427889);
+        mapController.setCenter(startPoint);
     }
 
     private void setupSlidingPanel() {
+        slidingPanel.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                expandIconView.setFraction(slideOffset,true);
+            }
 
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
 
+            }
+        });
     }
 
     private void setupRecyclerView() {
-
         LinearLayoutManager mLayoutManager = new GridLayoutManager(this, 3);
         recyclerView.setLayoutManager(mLayoutManager);
         SectionAdapter sectionAdapter = new SectionAdapter(R.layout.square_image_title, R.layout.list_section_header, MySection.getMapDataCatergorySections());
