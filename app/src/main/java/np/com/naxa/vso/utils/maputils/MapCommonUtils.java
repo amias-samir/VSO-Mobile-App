c41937fec75f3b824a3e9b5795dce3fa8c91a49b
Samir Dangal <smdangal201@gmail.com>
2018-07-31 11:49:42 +0545

    bounding box updated

--- app/src/main/java/np/com/naxa/vso/utils/maputils/MapCommonUtils.java
+++ app/src/main/java/np/com/naxa/vso/utils/maputils/MapCommonUtils.java
@@ -7,7 +7,7 @@ import org.osmdroid.views.MapView;
 public class MapCommonUtils {
 
     public static void zoomToMapBoundary(final MapView mapView,final GeoPoint centerPoint){
-        BoundingBox boundingBox = new BoundingBox(27.728708, 85.525139, 27.656069, 85.396133);
+        BoundingBox boundingBox = new BoundingBox(27.737334, 85.534334, 27.647866, 85.386996);
         mapView.zoomToBoundingBox(boundingBox, true);
         mapView.setScrollableAreaLimitDouble(boundingBox);
         mapView.getController().animateTo(centerPoint);
