ab373e670fac48ffd0f5754dbbc63736d6ec1b7b
Nishon Tandukar <nishon.tan@gmail.com>
2018-08-15 22:33:14 +0545

    Removed unused code

--- app/src/main/java/np/com/naxa/vso/activity/MapActivity.java
+++ app/src/main/java/np/com/naxa/vso/activity/MapActivity.java
@@ -18,7 +18,6 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.FolderOverlay;
-import org.osmdroid.views.overlay.Polygon;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -38,14 +37,12 @@ import np.com.naxa.vso.R;
 import np.com.naxa.vso.home.VSO;
 import np.com.naxa.vso.osm_convertor.multipolygon.Feature;
 import np.com.naxa.vso.osm_convertor.multipolygon.FeatureCollection;
-import np.com.naxa.vso.osm_convertor.multipolygon.Geometry;
 import np.com.naxa.vso.osm_convertor.multipolygon.Properties;
 import np.com.naxa.vso.osm_convertor.polygon.Properties_;
 import np.com.naxa.vso.utils.DialogFactory;
 import np.com.naxa.vso.utils.ToastUtils;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
-import timber.log.Timber;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -69,43 +66,14 @@ public class MapActivity extends AppCompatActivity {
             loadOSM();
             setupMapUI();
 
-
-//            loadGeoJSON("").subscribe(loadGeoJSONSubscriber());
-            loadString().subscribe(loadStringSubscriber());
+            loadGeoJSON("wards.geojson").subscribe(loadStringSubscriber());
         } else {
             EasyPermissions.requestPermissions(this, "Some rationale",
                     REQUEST_WRITE_PERMISSION, Manifest.permission.WRITE_EXTERNAL_STORAGE);
         }
     }
 
-    private Observer<? super Polygon> loadStringSubscriber() {
-        return new Observer<Polygon>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-
-            }
-
-            @Override
-            public void onNext(Polygon json) {
-                ToastUtils.showToast("GeoJSON string read :)");
-                map.getOverlays().add(json);
-
-
-            }
-
-            @Override
-            public void onError(Throwable e) {
-                DialogFactory.createGenericErrorDialog(MapActivity.this, e.getMessage()).show();
-            }
-
-            @Override
-            public void onComplete() {
-
-            }
-        };
-    }
-
-    private Observer<? super KmlDocument> loadGeoJSONSubscriber() {
+    private Observer<? super KmlDocument> loadStringSubscriber() {
         return new Observer<KmlDocument>() {
             @Override
             public void onSubscribe(Disposable d) {
@@ -114,10 +82,10 @@ public class MapActivity extends AppCompatActivity {
 
             @Override
             public void onNext(KmlDocument kmlDocument) {
-
+                ToastUtils.showToast("GeoJSON string read :)");
                 putKmlInMap(map, kmlDocument);
 
-                ToastUtils.showToast("GeoJSON plotted on map :)");
+
             }
 
             @Override
@@ -131,6 +99,7 @@ public class MapActivity extends AppCompatActivity {
             }
         };
     }
+
 
     private void loadOSM() {
         Context ctx = getApplicationContext();
@@ -145,78 +114,24 @@ public class MapActivity extends AppCompatActivity {
         mapController.setCenter(centerPoint);
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        map.onResume();
-    }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        map.onPause();
-    }
+    public Observable<KmlDocument> loadGeoJSON(String fileName) {
 
-    public void putKmlInMap(@NonNull MapView map, KmlDocument kmlDocument) {
-        KmlFeature.Styler styler = new OverlayPopupHiddenStyler();
-        FolderOverlay overlay = (FolderOverlay) kmlDocument.mKmlRoot.buildOverlay(map, null, styler, kmlDocument);
-        map.getOverlays().add(overlay);
-
-    }
-
-
-    public Observable<Polygon> loadString() {
-
-        return Observable.create(new ObservableOnSubscribe<Polygon>() {
+        return Observable.create(new ObservableOnSubscribe<KmlDocument>() {
             @Override
-            public void subscribe(ObservableEmitter<Polygon> e) throws Exception {
+            public void subscribe(ObservableEmitter<KmlDocument> e) throws Exception {
                 try {
-                    String str = readStringFromAsset("wards.geojson");
-                    FeatureCollection featureCollection = new Gson().fromJson(str, FeatureCollection.class);
+                    String str = readStringFromAsset(fileName);
 
+                    boolean isMultipolygon = str.contains("Multipolygon");
 
-                    for (Feature feature : featureCollection.getFeatures()) {
-                        String type = feature.getGeometry().getType();
-                        Properties props = feature.getProperties();
-
-                        switch (type) {
-                            case "MultiPolygon":
-                                for (int i = 0; i < feature.getGeometry().getCoordinates().size(); i++) {
-                                    np.com.naxa.vso.osm_convertor.polygon.Feature feature1 = new np.com.naxa.vso.osm_convertor.polygon.Feature();
-
-                                    np.com.naxa.vso.osm_convertor.polygon.Geometry geometry = new np.com.naxa.vso.osm_convertor.polygon.Geometry();
-                                    geometry.setCoordinates(feature.getGeometry().getCoordinates().get(i));
-                                    geometry.setType("Polygon");
-
-                                    feature1.setType("Feature");
-                                    feature1.setGeometry(geometry);
-
-
-                                    Properties_ p = new Properties_();
-                                    p.setAreaSqm(11);
-
-                                    feature1.setProperties(p);
-
-                                    List<np.com.naxa.vso.osm_convertor.polygon.Feature> featureList = new ArrayList<>();
-                                    featureList.add(feature1);
-
-
-                                    np.com.naxa.vso.osm_convertor.polygon.FeatureCollection featureCollection1 = new np.com.naxa.vso.osm_convertor.polygon.FeatureCollection();
-
-                                    featureCollection1.setType("FeatureCollection");
-                                    featureCollection1.setName("Wards");
-                                    featureCollection1.setFeatures(featureList);
-
-                                    String polygonStr = new Gson().toJson(featureCollection1);
-
-                                    Timber.i(polygonStr);
-                                }
-
-                                break;
-
-
-                        }
+                    if (isMultipolygon) {
+                        FeatureCollection featureCollection = new Gson().fromJson(str, FeatureCollection.class);
+                        mapMultilinePolygonToPolygon(featureCollection.getFeatures(), e);
+                    } else {
+                        e.onNext(generateOSMKML(fileName));
                     }
+
 
                     e.onComplete();
                 } catch (IOException ex) {
@@ -227,6 +142,78 @@ public class MapActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private KmlDocument generateOSMKML(String fileName) throws IOException {
+        InputStream jsonStream = null;
+        jsonStream = VSO.getInstance().getAssets().open(fileName);
+
+        int size = jsonStream.available();
+        byte[] buffer = new byte[size];
+        jsonStream.read(buffer);
+        jsonStream.close();
+        String jsonString = new String(buffer, "UTF-8");
+
+        KmlDocument kmlDocument = new KmlDocument();
+        kmlDocument.parseGeoJSON(jsonString);
+
+        return kmlDocument;
+    }
+
+    private void mapMultilinePolygonToPolygon(List<Feature> features, ObservableEmitter<KmlDocument> e) {
+
+
+        for (Feature feature : features) {
+            String type = feature.getGeometry().getType();
+            Properties props = feature.getProperties();
+
+            switch (type) {
+                case "MultiPolygon":
+                    for (int i = 0; i < feature.getGeometry().getCoordinates().size(); i++) {
+                        np.com.naxa.vso.osm_convertor.polygon.Feature feature1 = new np.com.naxa.vso.osm_convertor.polygon.Feature();
+
+                        np.com.naxa.vso.osm_convertor.polygon.Geometry geometry = new np.com.naxa.vso.osm_convertor.polygon.Geometry();
+                        geometry.setCoordinates(feature.getGeometry().getCoordinates().get(i));
+                        geometry.setType("Polygon");
+
+                        feature1.setType("Feature");
+                        feature1.setGeometry(geometry);
+
+
+                        Properties_ p = new Properties_();
+                        p.setAreaSqm(11);
+
+                        feature1.setProperties(p);
+
+                        List<np.com.naxa.vso.osm_convertor.polygon.Feature> featureList = new ArrayList<>();
+                        featureList.add(feature1);
+
+
+                        np.com.naxa.vso.osm_convertor.polygon.FeatureCollection featureCollection1 = new np.com.naxa.vso.osm_convertor.polygon.FeatureCollection();
+
+                        featureCollection1.setType("FeatureCollection");
+                        featureCollection1.setName("Wards");
+                        featureCollection1.setFeatures(featureList);
+
+                        String polygonStr = new Gson().toJson(featureCollection1);
+
+
+                        KmlDocument kmlDocument = new KmlDocument();
+                        boolean isSucess = kmlDocument.parseGeoJSON(polygonStr);
+
+
+                        if (!isSucess) {
+                            throw new RuntimeException("Failed to parse geojson");
+                        }
+
+                        e.onNext(kmlDocument);
+                    }
+
+                    break;
+
+
+            }
+        }
     }
 
     private String readStringFromAsset(String fileName) throws IOException {
@@ -241,39 +228,25 @@ public class MapActivity extends AppCompatActivity {
         return jsonString;
     }
 
-    public io.reactivex.Observable<KmlDocument> loadGeoJSON(String fileName) {
 
-        return io.reactivex.Observable.create(e -> {
-            try {
-
-                InputStream jsonStream = null;
-//                jsonStream = VSO.getInstance().getAssets().open("wards.geojson");
-                jsonStream = VSO.getInstance().getAssets().open("changunarayan_new_wards.geojson");
-
-                int size = jsonStream.available();
-                byte[] buffer = new byte[size];
-                jsonStream.read(buffer);
-                jsonStream.close();
-                String jsonString = new String(buffer, "UTF-8");
-
-                KmlDocument kmlDocument = new KmlDocument();
-                boolean isSucess = kmlDocument.parseGeoJSON(jsonString);
-
-
-                if (!isSucess) {
-                    throw new RuntimeException("Failed to parse geojson");
-                }
-
-                e.onNext(kmlDocument);
-
-                e.onComplete();
-            } catch (IOException ex) {
-                ex.printStackTrace();
-                e.onError(ex);
-            }
-
-        });
-
-
+    public void putKmlInMap(@NonNull MapView map, KmlDocument kmlDocument) {
+        KmlFeature.Styler style = new OverlayPopupHiddenStyler();
+        FolderOverlay overlay = (FolderOverlay) kmlDocument.mKmlRoot.buildOverlay(map, null, style, kmlDocument);
+        map.getOverlays().add(overlay);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        map.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        map.onPause();
+    }
+
+
+
 }
